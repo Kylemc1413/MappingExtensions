22672e48dd4466ff25d502423a82456825cd82fa
Kyle1413 <kylet1412@gmail.com>
2019-01-27 10:36:44 -0500

    Add project files.

---
+++ MappingExtensions/Plugin.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using IllusionPlugin;
+
+
+namespace MappingExtensions
+{
+    public class Plugin : IPlugin
+    {
+        public string Name => "Plugin Name";
+        public string Version => "0.0.1";
+
+        bool doesPluginExist;
+
+        public void OnApplicationStart()
+        {
+            SceneManager.activeSceneChanged += SceneManagerOnActiveSceneChanged;
+            SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+
+            //Checks if a IPlugin with the name in quotes exists, in case you want to verify a plugin exists before trying to reference it, or change how you do things based on if a plugin is present
+            doesPluginExist = IllusionInjector.PluginManager.Plugins.Any(x => x.Name == "Saber Mod");
+
+
+        }
+
+        private void SceneManagerOnActiveSceneChanged(Scene oldScene, Scene newScene)
+        {
+
+            if (newScene.name == "Menu")
+            {
+                //Code to execute when entering The Menu
+
+
+            }
+
+            if (newScene.name == "GameCore")
+            {
+                //Code to execute when entering actual gameplay
+
+
+            }
+
+
+        }
+
+        private void SceneManager_sceneLoaded(Scene scene, LoadSceneMode arg1)
+        {
+            //Create GameplayOptions/SettingsUI if using either
+            if (scene.name == "Menu")
+                UI.BasicUI.CreateUI();
+
+        }
+
+        public void OnApplicationQuit()
+        {
+            SceneManager.activeSceneChanged -= SceneManagerOnActiveSceneChanged;
+            SceneManager.sceneLoaded -= SceneManager_sceneLoaded;
+        }
+
+        public void OnLevelWasLoaded(int level)
+        {
+
+        }
+
+        public void OnLevelWasInitialized(int level)
+        {
+        }
+
+        public void OnUpdate()
+        {
+
+
+        }
+
+        public void OnFixedUpdate()
+        {
+        }
+    }
+}
---
+++ MappingExtensions/UI/BasicUI.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomUI.GameplaySettings;
+using CustomUI.Settings;
+
+namespace MappingExtensions.UI
+{
+    class BasicUI
+    {
+        public static void CreateUI()
+        {
+            //This will create the UI for the plugin when called, keep in mind that the mod will require CustomUI when executing this as it calls functions etc from the library
+            CreateGameplayOptionsUI();
+            CreateSettingsUI();
+
+
+
+        }
+
+
+
+        public static void CreateSettingsUI()
+        {
+            //This will create a menu tab in the settings menu for your plugin
+            var pluginSettingsSubmenu = SettingsUI.CreateSubMenu("Submenu Name");
+
+            var exampleToggle = pluginSettingsSubmenu.AddBool("Example Toggle");
+            //Fetch your initial value for the option from within the braces, or simply have it default to a value
+            exampleToggle.GetValue += delegate { return false; };
+            exampleToggle.SetValue += delegate (bool value)
+            {
+                //Whatever execution you want to occur after setting the value
+
+            };
+
+        }
+
+        public static void CreateGameplayOptionsUI()
+        {
+
+            //Example submenu option
+            var pluginSubmenu = GameplaySettingsUI.CreateSubmenuOption(GameplaySettingsPanels.ModifiersLeft, "Plugin Name", "MainMenu", "pluginMenu1", "You can keep all your plugin's gameplay options nested within this one button");
+
+            //Example Toggle Option within a submenu
+            var exampleOption = GameplaySettingsUI.CreateToggleOption(GameplaySettingsPanels.ModifiersLeft, "Example Toggle", "pluginMenu1", "Put a toggle for a setting you want easily accessible in game here.");
+            exampleOption.GetValue = /* Fetch the initial value for the option here*/ false;
+            exampleOption.OnToggle += (value) => { /*  You can execute whatever you want to occur when the value is toggled here, usually that would include updating wherever the value is pulled from   */};
+            exampleOption.AddConflict("Conflicting Option Name"); //You can add conflicts with other gameplay options settings here, preventing both from being active at the same time, including that of other mods
+
+
+
+        }
+
+
+
+
+
+    }
+}
