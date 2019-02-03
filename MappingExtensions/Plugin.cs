16649c168b1bf7dad320f6d8f684d6f17bd66111
Kyle1413 <kylet1412@gmail.com>
2019-02-03 16:41:23 -0500

    Version

--- MappingExtensions/Plugin.cs
+++ MappingExtensions/Plugin.cs
@@ -15,7 +15,7 @@ namespace MappingExtensions
     public class Plugin : IPlugin
     {
         public string Name => "Mapping Extensions";
-        public string Version => "1.0.0";
+        public string Version => "1.0.1";
         public static HarmonyInstance harmony;
         bool doesPluginExist;
 
