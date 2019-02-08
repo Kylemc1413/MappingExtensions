9c062e93feaa6eb305800d6874168b294ec0caf0
Kyle1413 <kylet1412@gmail.com>
2019-02-07 21:57:55 -0500

    Simplify direction mirroring

--- MappingExtensions/HarmonyPatches/NoteDataMirrorTransformCutDirection.cs
+++ MappingExtensions/HarmonyPatches/NoteDataMirrorTransformCutDirection.cs
@@ -25,12 +25,7 @@ new Type[] {
             if ((int)__state >= 1000)
             {
                 int cutdir = (int)__state;
-                int angle = cutdir - 1000;
-
-                angle = angle > 180 ? ((angle - 360) * -1) : 360 - angle;
-
-                int newdir = angle + 1000;
-
+                int newdir = 2360 - cutdir;
                 __instance.SetProperty("cutDirection", (NoteCutDirection)newdir);
             }
 
