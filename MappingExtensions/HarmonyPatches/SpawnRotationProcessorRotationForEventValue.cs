1ce3c42dd35df22288ca8df87647794e75703c04
Kyle1413 <kylet1412@gmail.com>
2020-02-11 21:53:24 -0500

    Precision Rotation event support

---
+++ MappingExtensions/HarmonyPatches/SpawnRotationProcessorRotationForEventValue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+namespace MappingExtensions.HarmonyPatches
+{
+    [HarmonyPatch(typeof(SpawnRotationProcessor))]
+    [HarmonyPatch("RotationForEventValue", MethodType.Normal)]
+    class SpawnRotationProcessorRotationForEventValue
+    {
+        static void Postfix(int index, ref float __result)
+        {
+            if (!Plugin.active) return;
+            if (!BS_Utils.Plugin.LevelData.GameplayCoreSceneSetupData.difficultyBeatmap.parentDifficultyBeatmapSet.beatmapCharacteristic.requires360Movement) return;
+            if (index >= 1000 && index <= 1720)
+                __result = index - 1360;
+        }
+    }
+}
